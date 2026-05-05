ad96eed3a07f6654c357972d8fc7bba1dd4b083c
im-a-Cloud <prudencioalvaro10@gmail.com>
2026-05-05 16:36:38 -0300

    Third Commit: Skills

---
+++ src/main/java/com/example/RPG_Manager20/Model/DTO/PericiaDTO.java
@@ -0,0 +1,9 @@
+package com.example.RPG_Manager20.Model.DTO;
+
+import com.example.RPG_Manager20.Model.Enums.Atributos;
+
+public record PericiaDTO(
+        String nomePericia,
+        Atributos atributoChave,
+        int valorTotal
+){}
---
+++ src/main/java/com/example/RPG_Manager20/Model/Entities/Pericia.java
@@ -0,0 +1,46 @@
+package com.example.RPG_Manager20.Model.Entities;
+
+import com.example.RPG_Manager20.Model.Enums.Atributos;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pericia")
+public class Pericia extends AbstractModel{
+    private String nomePericia;
+    private Atributos atributoChave;
+    private int valorTotal;
+
+    public Pericia() {
+    }
+
+    public Pericia(String nomePericia, Atributos atributoChave, int valorTotal) {
+        this.nomePericia = nomePericia;
+        this.atributoChave = atributoChave;
+        this.valorTotal = valorTotal;
+    }
+
+    public String getNomePericia() {
+        return nomePericia;
+    }
+
+    public void setNomePericia(String nomePericia) {
+        this.nomePericia = nomePericia;
+    }
+
+    public Atributos getAtributoChave() {
+        return atributoChave;
+    }
+
+    public void setAtributoChave(Atributos atributoChave) {
+        this.atributoChave = atributoChave;
+    }
+
+    public int getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(int valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
